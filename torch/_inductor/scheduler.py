fb72ad4a4eececab908ad48bb052bd55a3cc5688
xinan.lin <xinan.lin@intel.com>
2025-11-04 13:51:10 +0000

    [Inductor XPU GEMM] Step 5/N: Refactor CUDACombinedScheduling and  CUDACppScheduling.
    ghstack-source-id: dd101b5510ebc3ecd8012098695d9035e71a55fe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/160688

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -5849,10 +5849,10 @@ class Scheduler:
                 node = typing.cast(ForeachKernelSchedulerNode, node)
                 # pyrefly: ignore [unbound-name]
                 backend_ = self.get_backend(device)
-                from .codegen.cuda_combined_scheduling import CUDACombinedScheduling
+                from .codegen.combined_scheduling import CombinedScheduling
                 from .codegen.simd import SIMDScheduling
 
-                if isinstance(backend_, (SIMDScheduling, CUDACombinedScheduling)):
+                if isinstance(backend_, (SIMDScheduling, CombinedScheduling)):
                     backend = backend_
                 else:
                     raise AssertionError(f"{type(self)=}")
